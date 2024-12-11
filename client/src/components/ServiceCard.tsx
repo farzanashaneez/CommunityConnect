a46835be3a86b18f8a2a36d83552f0480cadf8ca
farzanashaneez <farzanashaneez@gmail.com>
2024-12-11 13:19:53 +0400

    added service detailing

--- client/src/components/ServiceCard.tsx
+++ client/src/components/ServiceCard.tsx
@@ -44,6 +44,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   const { deleteService, updateService } = useCommunityContext();
 
   const [isEditDialogOpen, setEditDialogOpen] = useState(false);
+  const [isDetailsDialogOpen, setDetailsDialogOpen] = useState(false);
 
   const handleDelete = async (id: string) => {
     try {
@@ -58,8 +59,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
 
   const handleEditSubmit = async (values: Service) => {
     try {
-      console.log('values',service)
-      const updatedService = await updateServiceApi(values._id,values)
+      const updatedService = await updateServiceApi(values._id, values);
       updateService(updatedService); // Update context with new data
       showSnackbar("Service updated successfully", "success");
       setEditDialogOpen(false);
@@ -70,10 +70,13 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   };
 
   const handleEdit = () => setEditDialogOpen(true);
+  const handleDetailsOpen = () => setDetailsDialogOpen(true);
+  const handleDetailsClose = () => setDetailsDialogOpen(false);
 
   return (
     <>
       <Card
+        onClick={handleDetailsOpen} // Open details popup when card is clicked
         sx={{
           mb: 2,
           p: 2,
@@ -83,6 +86,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
           width: "100%",
           maxWidth: { xs: "100%", sm: "300px" },
           position: "relative",
+          cursor: "pointer", // Indicate clickable area
         }}
       >
         {isAdmin && (
@@ -99,7 +103,10 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
               aria-label="edit"
               size="small"
               color="primary"
-              onClick={handleEdit}
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent triggering card click
+                handleEdit();
+              }}
             >
               <EditIcon fontSize="small" />
             </IconButton>
@@ -107,7 +114,10 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
               aria-label="delete"
               size="small"
               color="error"
-              onClick={() => handleDelete(service._id)}
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent triggering card click
+                handleDelete(service._id);
+              }}
             >
               <DeleteIcon fontSize="small" />
             </IconButton>
@@ -148,12 +158,16 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
           >
             {service.description}
           </Typography>
-       
-          {service.price===0? <Typography variant="body2" sx={{ fontWeight: "bold", mb: 0 }}>
-            Charge:Free
-          </Typography>: <Typography variant="body2" sx={{ fontWeight: "bold", mb: 0 }}>
-            Charge:{service.price}-aed
-          </Typography>}
+
+          {service.price === 0 ? (
+            <Typography variant="body2" sx={{ fontWeight: "bold", mb: 0 }}>
+              Charge: Free
+            </Typography>
+          ) : (
+            <Typography variant="body2" sx={{ fontWeight: "bold", mb: 0 }}>
+              Charge: {service.price}-aed
+            </Typography>
+          )}
         </CardContent>
         <CustomSnackbar
           open={snackbar.open}
@@ -163,86 +177,101 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
         />
       </Card>
 
-      {/* Edit Dialog */}
-      <Dialog open={isEditDialogOpen} onClose={() => setEditDialogOpen(false)}>
-  <DialogTitle>Edit Service</DialogTitle>
-  <Formik
-    initialValues={{
-      ...service, // Include _id in initial values
-    }}
-    validationSchema={Yup.object({
-      name: Yup.string().required("Name is required"),
-      description: Yup.string().required("Description is required"),
-      price: Yup.number()
-        .required("Price is required")
-        .min(0, "Price cannot be negative"),
-      imageUrl: Yup.string().url("Must be a valid URL"),
-    })}
-    onSubmit={(values) => handleEditSubmit(values)}
-  >
-    {({ values, errors, touched, handleChange }) => (
-      <Form>
+      {/* Details Dialog */}
+      <Dialog open={isDetailsDialogOpen} onClose={handleDetailsClose}>
+        <DialogTitle>{service.name}</DialogTitle>
         <DialogContent>
-          {/* Display current image */}
-          {values.imageUrl && (
+          {service.imageUrl && (
             <CardMedia
               component="img"
               sx={{
-                width: "100px",
-                height: "100px",
+                width: "100%",
+                height: "200px",
                 objectFit: "cover",
-                margin: "0 auto 16px",
                 borderRadius: "8px",
+                mb: 2,
               }}
-              image={values.imageUrl}
-              alt="Service Image"
+              image={service.imageUrl}
+              alt={service.name}
             />
           )}
-          <TextField
-            name="name"
-            label="Name"
-            fullWidth
-            value={values.name}
-            onChange={handleChange}
-            error={touched.name && Boolean(errors.name)}
-            helperText={touched.name && errors.name}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            name="description"
-            label="Description"
-            fullWidth
-            multiline
-            value={values.description}
-            onChange={handleChange}
-            error={touched.description && Boolean(errors.description)}
-            helperText={touched.description && errors.description}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            name="price"
-            label="Price"
-            fullWidth
-            type="number"
-            value={values.price}
-            onChange={handleChange}
-            error={touched.price && Boolean(errors.price)}
-            helperText={touched.price && errors.price}
-            sx={{ mb: 2 }}
-          />
-         
+          <Typography variant="body1" gutterBottom>
+            {service.description}
+          </Typography>
+          <Typography variant="body2">
+            {service.price === 0 ? "Charge: Free" : `Charge: ${service.price}-aed`}
+          </Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setEditDialogOpen(false)}>Cancel</Button>
-          <Button type="submit" variant="contained" color="primary">
-            Save
+          <Button onClick={handleDetailsClose} color="primary">
+            Close
           </Button>
         </DialogActions>
-      </Form>
-    )}
-  </Formik>
-</Dialog>
+      </Dialog>
 
+      {/* Edit Dialog */}
+      <Dialog open={isEditDialogOpen} onClose={() => setEditDialogOpen(false)}>
+        <DialogTitle>Edit Service</DialogTitle>
+        <Formik
+          initialValues={{
+            ...service, // Include _id in initial values
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string().required("Name is required"),
+            description: Yup.string().required("Description is required"),
+            price: Yup.number()
+              .required("Price is required")
+              .min(0, "Price cannot be negative"),
+            imageUrl: Yup.string().url("Must be a valid URL"),
+          })}
+          onSubmit={(values) => handleEditSubmit(values)}
+        >
+          {({ values, errors, touched, handleChange }) => (
+            <Form>
+              <DialogContent>
+                <TextField
+                  name="name"
+                  label="Name"
+                  fullWidth
+                  value={values.name}
+                  onChange={handleChange}
+                  error={touched.name && Boolean(errors.name)}
+                  helperText={touched.name && errors.name}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="description"
+                  label="Description"
+                  fullWidth
+                  multiline
+                  value={values.description}
+                  onChange={handleChange}
+                  error={touched.description && Boolean(errors.description)}
+                  helperText={touched.description && errors.description}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  name="price"
+                  label="Price"
+                  fullWidth
+                  type="number"
+                  value={values.price}
+                  onChange={handleChange}
+                  error={touched.price && Boolean(errors.price)}
+                  helperText={touched.price && errors.price}
+                  sx={{ mb: 2 }}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => setEditDialogOpen(false)}>Cancel</Button>
+                <Button type="submit" variant="contained" color="primary">
+                  Save
+                </Button>
+              </DialogActions>
+            </Form>
+          )}
+        </Formik>
+      </Dialog>
     </>
   );
 };
